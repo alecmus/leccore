alecmus/leccore

commit c8e2d0aecdcd7b2fa8a4c1194b2e20f8b489e36c

Improvements by simplifying blob type.

database/sqlcipher/sqlcipher_connection.cpp
@@ -154,13 +154,12 @@ bool sqlcipher_connection::execute(const std::string& sql,
 		// do some binding
 		// supported types: int, float, double, text(const char*, std::string), blob(database::blob)
 		// 
-		std::vector<blob> blob_data;	// to keep blob data until sqlite3_step() is executed
 		int index = 1;
 		for (auto& value : values) {
 			if (value.has_value()) {
 				// bind integers (int)
 				if (value.type() == typeid(int)) {
-					const auto integer = std::any_cast<int>(value);
+					auto integer = std::any_cast<int>(value);
 
 					if (sqlite3_bind_int(statement, index, integer) != SQLITE_OK) {
 						error = d_.sqlite_error();
@@ -171,7 +170,7 @@ bool sqlcipher_connection::execute(const std::string& sql,
 
 				// bind floats (float)
 				if (value.type() == typeid(float)) {
-					const auto f = std::any_cast<float>(value);
+					auto f = std::any_cast<float>(value);
 
 					if (sqlite3_bind_double(statement, index, f) != SQLITE_OK) {
 						error = d_.sqlite_error();
@@ -182,7 +181,7 @@ bool sqlcipher_connection::execute(const std::string& sql,
 
 				// bind doubles (double)
 				if (value.type() == typeid(double)) {
-					const auto d = std::any_cast<double>(value);
+					auto d = std::any_cast<double>(value);
 
 					if (sqlite3_bind_double(statement, index, d) != SQLITE_OK) {
 						error = d_.sqlite_error();
@@ -193,8 +192,8 @@ bool sqlcipher_connection::execute(const std::string& sql,
 
 				// bind text (const char*)
 				if (value.type() == typeid(const char*)) {
-					const char* buffer = std::any_cast<const char*>(value);
-					const auto length = (int)strlen(buffer);
+					auto buffer = std::any_cast<const char*>(value);
+					auto length = (int)strlen(buffer);
 
 					if (sqlite3_bind_text(statement, index, buffer, length, SQLITE_STATIC) != SQLITE_OK) {
 						error = d_.sqlite_error();
@@ -205,9 +204,9 @@ bool sqlcipher_connection::execute(const std::string& sql,
 
 				// bind text (std::string)
 				if (value.type() == typeid(std::string)) {
-					const std::string data = std::any_cast<std::string>(value);
-					const char* buffer = data.c_str();
-					const auto length = (int)data.length();
+					auto data = std::any_cast<std::string>(value);
+					char* buffer = (char*)data.c_str();
+					auto length = (int)data.length();
 
 					if (sqlite3_bind_text(statement, index, buffer, length, SQLITE_STATIC) != SQLITE_OK) {
 						error = d_.sqlite_error();
@@ -218,10 +217,9 @@ bool sqlcipher_connection::execute(const std::string& sql,
 
 				// bind blob (database::blob)
 				if (value.type() == typeid(blob)) {
-					blob_data.push_back(std::any_cast<blob>(value));
-					const std::string& data = blob_data.back().get();
-					const char* buffer = data.c_str();
-					const auto size = (int)data.length();
+					auto data = std::any_cast<blob>(value).data;
+					char* buffer = (char*)data.c_str();
+					auto size = (int)data.length();
 
 					if (sqlite3_bind_blob(statement, index, buffer, size, SQLITE_STATIC) != SQLITE_OK) {
 						error = d_.sqlite_error();
@@ -318,7 +316,7 @@ bool sqlcipher_connection::execute_query(const std::string& sql, table& results,
 
 							read_blob(&buffer, &size);
 							std::string value(buffer, size);
-							blob b(value);
+							blob b{ value };
 							current_row.insert(std::make_pair(column_name, b));
 
 							free(buffer);

database/connection.cpp
@@ -14,28 +14,6 @@
 
 using namespace liblec::leccore::database;
 
-class blob::blob_impl {
-public:
-	const std::string data_;
-	blob_impl(const std::string& data) :
-		data_(data) {}
-	~blob_impl() {}
-};
-
-blob::blob(const std::string& data) :
-	d_(*new blob_impl(data)) {}
-
-blob::~blob() {
-	delete& d_;
-}
-
-blob::blob(const blob& obj) :
-	d_(*new blob_impl(obj.d_.data_)) {}
-
-const std::string& blob::get() {
-	return d_.data_;
-}
-
 class connection::impl {
 public:
 	impl(const std::string& type,

database/connection.h
@@ -24,16 +24,8 @@
 namespace liblec {
 	namespace leccore {
 		namespace database {
-			class leccore_api blob {
-			public:
-				blob(const std::string& data);
-				~blob();
-				blob(const blob& obj);
-				const std::string& get();
-
-			private:
-				class blob_impl;
-				blob_impl& d_;
+			struct blob {
+				std::string data;
 			};
 
 			class leccore_api get {

commit 7d35081c75555d3279f7de850374acba8b2d34ae

Fixed bug introduced in c8e2d0aecdcd7b2fa8a4c1194b2e20f8b489e36c: blob data needs to be kept until sqlite3_step() is executed.

database/sqlcipher/sqlcipher_connection.cpp
@@ -168,6 +168,7 @@ bool sqlcipher_connection::execute(const std::string& sql,
 		// do some binding
 		// supported types: int, float, double, text(const char*, std::string), blob(database::blob)
 		// 
+		std::vector<std::string> blob_data;	// to keep blob data until sqlite3_step() is executed
 		int index = 1;
 		for (auto& value : values) {
 			if (value.has_value()) {
@@ -231,7 +232,8 @@ bool sqlcipher_connection::execute(const std::string& sql,
 
 				// bind blob (database::blob)
 				if (value.type() == typeid(blob)) {
-					auto data = std::any_cast<blob>(value).data;
+					blob_data.push_back(std::any_cast<blob>(value).data);
+					std::string& data = blob_data.back();
 					char* buffer = (char*)data.c_str();
 					auto size = (int)data.length();
 