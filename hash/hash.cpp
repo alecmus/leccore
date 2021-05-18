b0272a2893728193f1e2270789971ae8720d6c11
Alec Musasa <alecmus@live.com>
2021-05-19 01:39:48 +0200

    Switched to cryptopp for hashing.

--- hash/hash.cpp
+++ hash/hash.cpp
@@ -9,37 +9,42 @@
 //
 
 #include "../hash.h"
-#include "sha256.h"
-#include "sha512.h"
+#include <cryptlib.h>
+#include <sha.h>
+#include <hex.h>
 
 std::string liblec::leccore::hash::sha256(const std::string& input) {
-    unsigned char digest[SHA256::DIGEST_SIZE];
-    memset(digest, 0, SHA256::DIGEST_SIZE);
-
-    SHA256 ctx = SHA256();
-    ctx.init();
-    ctx.update((unsigned char*)input.c_str(), static_cast<unsigned int>(input.length()));
-    ctx.final(digest);
-
-    char buf[2 * SHA256::DIGEST_SIZE + 1];
-    buf[2 * SHA256::DIGEST_SIZE] = 0;
-    for (int i = 0; i < SHA256::DIGEST_SIZE; i++)
-        sprintf_s(buf + i * 2, 2 + 1, "%02x", digest[i]);
-    return std::string(buf);
+    try {
+        CryptoPP::SHA256 hash;
+        std::string output;
+        CryptoPP::StringSource ss(input, true,
+            new CryptoPP::HashFilter(hash,
+                new CryptoPP::HexEncoder(new CryptoPP::StringSink(output))));
+        return output;
+    }
+    catch (CryptoPP::Exception&) {
+        // to-do: log
+    }
+    catch (std::exception&) {
+        // to-do: log
+    }
+    return std::string();
 }
 
 std::string liblec::leccore::hash::sha512(const std::string& input) {
-    unsigned char digest[SHA512::DIGEST_SIZE];
-    memset(digest, 0, SHA512::DIGEST_SIZE);
-
-    SHA512 ctx = SHA512();
-    ctx.init();
-    ctx.update((unsigned char*)input.c_str(), static_cast<unsigned int>(input.length()));
-    ctx.final(digest);
-
-    char buf[2 * SHA512::DIGEST_SIZE + 1];
-    buf[2 * SHA512::DIGEST_SIZE] = 0;
-    for (int i = 0; i < SHA512::DIGEST_SIZE; i++)
-        sprintf_s(buf + i * 2, 2 + 1, "%02x", digest[i]);
-    return std::string(buf);
+    try {
+        CryptoPP::SHA512 hash;
+        std::string output;
+        CryptoPP::StringSource ss(input, true,
+            new CryptoPP::HashFilter(hash,
+                new CryptoPP::HexEncoder(new CryptoPP::StringSink(output))));
+        return output;
+    }
+    catch (CryptoPP::Exception&) {
+        // to-do: log
+    }
+    catch (std::exception&) {
+        // to-do: log
+    }
+    return std::string();
 }
