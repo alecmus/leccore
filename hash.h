0d62cfcb9263e30898f21b882b98454f4d0bed0b
Alec Musasa <alecmus@live.com>
2021-05-18 16:22:11 +0200

    Added the hash class, and added sha256 to it.

---
+++ hash.h
@@ -0,0 +1,28 @@
+//
+// hash.h - hash interface
+//
+// leccore library, part of the liblec library
+// Copyright (c) 2019 Alec Musasa (alecmus at live dot com)
+//
+// Released under the CC-BY-NC 2.0 license. For full details see the file
+// LICENSE.txt or go to https://github.com/alecmus/liblec/blob/master/LICENSE.md
+//
+
+#pragma once
+
+#if defined(LECCORE_EXPORTS)
+#include "leccore.h"
+#else
+#include <liblec/leccore.h>
+#endif
+
+#include <string>
+
+namespace liblec {
+	namespace leccore {
+		class leccore_api hash {
+		public:
+			static std::string sha256(const std::string& input);
+		};
+	}
+}
---
+++ hash/hash.cpp
@@ -0,0 +1,28 @@
+//
+// hash.cpp - hash implementation
+//
+// leccore library, part of the liblec library
+// Copyright (c) 2019 Alec Musasa (alecmus at live dot com)
+//
+// Released under the CC-BY-NC 2.0 license. For full details see the file
+// LICENSE.txt or go to https://github.com/alecmus/liblec/blob/master/LICENSE.md
+//
+
+#include "..\hash.h"
+#include "sha256.h"
+
+std::string liblec::leccore::hash::sha256(const std::string& input) {
+    unsigned char digest[SHA256::DIGEST_SIZE];
+    memset(digest, 0, SHA256::DIGEST_SIZE);
+
+    SHA256 ctx = SHA256();
+    ctx.init();
+    ctx.update((unsigned char*)input.c_str(), static_cast<unsigned int>(input.length()));
+    ctx.final(digest);
+
+    char buf[2 * SHA256::DIGEST_SIZE + 1];
+    buf[2 * SHA256::DIGEST_SIZE] = 0;
+    for (int i = 0; i < SHA256::DIGEST_SIZE; i++)
+        sprintf_s(buf + i * 2, 2 + 1, "%02x", digest[i]);
+    return std::string(buf);
+}
