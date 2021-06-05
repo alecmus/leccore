3536bb3f536a7cfc9ca53e6a66b308f59bb2a872
Alec Musasa <alecmus@live.com>
2021-06-05 21:51:02 +0200

    Added unzip class.

--- zip.h
+++ zip.h
@@ -21,7 +21,7 @@
 
 namespace liblec {
 	namespace leccore {
-		/// <summary>Class for making zip files.</summary>
+		/// <summary>Class for zipping files and folders into a zip archive.</summary>
 		class leccore_api zip {
 		public:
 			zip();
@@ -51,6 +51,56 @@ namespace liblec {
 		private:
 			class impl;
 			impl& d_;
+
+			// Copying an object of this class is not allowed
+			zip(const zip&) = delete;
+			zip& operator=(const zip&) = delete;
+		};
+
+		/// <summary>Class for unzipping a zip archive.</summary>
+		class leccore_api unzip {
+		public:
+			unzip();
+			~unzip();
+
+			/// <summary>Unzip log.</summary>
+			using unzip_log = struct {
+				std::vector<std::string> error_list;
+			};
+
+			/// <summary>Start zipping operation.</summary>
+			/// <param name="filename">The full path to the zip archive, including the extension.</param>
+			/// <param name="directory">The directory to extract the zip archive to. Use an empty string to
+			/// extract to the current directory.</param>
+			/// <remarks>This method returns almost immediately. The actual unzipping is executed
+			/// on a seperate thread. To check the status of the unzipping call the
+			/// <see cref="unzipping"></see> method.</remarks>
+			void start(const std::string& filename,
+				const std::string& directory);
+
+			/// <summary>Check whether the unzipping operation is still underway.</summary>
+			/// <returns>Returns true if the unzipping is still underway, else false.</returns>
+			/// <remarks>After calling <see cref="start"></see> call this method in a loop or a timer, depending on
+			/// your kind of app, then call <see cref="result"></see> once it returns false.</remarks>
+			bool unzipping();
+
+			/// <summary>The result of the unzipping operation.</summary>
+			/// <param name="log">Unzip log as defined in the <see cref="unzip_log"></see> type. This may contain
+			/// error messages for individual entries regardless of whether the method returns true or false. For example
+			/// if one of the files already existed and was marked as read-only the unzipping operation continues
+			/// and this method will return true yet an error will have actually occurred for that one entry.</param>
+			/// <param name="error">Error information.</param>
+			/// <returns>Returns true if the operation was successful, else false. When false the
+			/// error information is written back to <see cref="error"></see>.</returns>
+			bool result(unzip_log& log, std::string& error);
+
+		private:
+			class impl;
+			impl& d_;
+
+			// Copying an object of this class is not allowed
+			unzip(const unzip&) = delete;
+			unzip& operator=(const unzip&) = delete;
 		};
 	}
 }
---
+++ zip/unzip.cpp
@@ -0,0 +1,181 @@
+//
+// unzip.cpp - unzip implementation
+//
+// leccore library, part of the liblec library
+// Copyright (c) 2019 Alec Musasa (alecmus at live dot com)
+//
+// Released under the CC-BY-NC 2.0 license. For full details see the file
+// LICENSE.txt or go to https://github.com/alecmus/liblec/blob/master/LICENSE.md
+//
+
+#include "../zip.h"
+#include <thread>
+#include <future>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+#define POCO_STATIC
+
+#include <Poco/AutoPtr.h>
+#include <Poco/Zip/Decompress.h>
+#include <Poco/Zip/ZipLocalFileHeader.h>
+#include <Poco/Zip/ZipArchive.h>
+#include <Poco/Path.h>
+#include <Poco/File.h>
+#include <Poco/Delegate.h>
+
+using Poco::Zip::ZipLocalFileHeader;
+using Poco::AutoPtr;
+
+using namespace liblec::leccore;
+
+class unzip::impl {
+public:
+	std::string filename_;
+	std::string directory_;
+	unzip_log log_;
+
+	struct unzip_result {
+		bool success = false;
+		std::string error;
+	};
+
+	std::future<unzip_result> fut_;
+
+	impl() {}
+	~impl() {}
+
+	void on_error(const void*, std::pair<const ZipLocalFileHeader, const std::string>& info) {
+		log_.error_list.push_back(info.second);
+	}
+
+	void on_ok(const void*, std::pair<const ZipLocalFileHeader, const Poco::Path>& info) {
+		std::string path = directory_ + info.second.toString(Poco::Path::PATH_WINDOWS);
+
+		// set file last modified time
+		Poco::File file(path);
+
+		Poco::DateTime dt(info.first.lastModifiedAt());
+		Poco::Timestamp::UtcTimeVal utc = dt.utcTime();
+		Poco::Timestamp ts = dt.timestamp();
+		file.setLastModified(ts);
+
+		// to-do: set file attributes
+	}
+
+	static unzip_result unzip_func(impl* p_impl) {
+		impl& d_ = *p_impl;
+
+		unzip_result result = {};
+
+		if (d_.filename_.empty()) {
+			result.error = "File not specified";
+			result.success = false;
+			return result;
+		}
+
+		try {
+			if (!d_.directory_.empty()) {
+				std::filesystem::path path(d_.directory_);
+				if (std::filesystem::exists(path) && !std::filesystem::is_directory(path)) {
+					result.error = "Invalid output directory";
+					result.success = false;
+					return result;
+				}
+			}
+
+			if (!d_.directory_.empty()) {
+				// add trailing slash if it's missing
+				if (d_.directory_[d_.directory_.length() - 1] != '\\')
+					d_.directory_ += "\\";
+
+				std::filesystem::path path(d_.directory_);
+				std::filesystem::create_directories(path);
+			}
+
+			std::ifstream in(d_.filename_, std::ios::binary);
+
+			if (!in.is_open()) {
+				result.error = "Error opening file";
+				result.success = false;
+				return result;
+			}
+
+			Poco::Zip::Decompress decompress(in, d_.directory_);
+			decompress.EError += Poco::Delegate<unzip::impl,
+				std::pair<const ZipLocalFileHeader, const std::string> >(p_impl, &unzip::impl::on_error);
+			decompress.EOk += Poco::Delegate<unzip::impl,
+				std::pair<const ZipLocalFileHeader, const Poco::Path>>(p_impl, &unzip::impl::on_ok);
+			decompress.decompressAllFiles();
+			decompress.EError -= Poco::Delegate<unzip::impl,
+				std::pair<const ZipLocalFileHeader, const std::string> >(p_impl, &unzip::impl::on_error);
+			decompress.EOk -= Poco::Delegate<unzip::impl,
+				std::pair<const ZipLocalFileHeader, const Poco::Path> >(p_impl, &unzip::impl::on_ok);
+
+			result.success = true;
+			return result;
+		}
+		catch (Poco::Exception& e) {
+			result.error = e.displayText();
+			result.success = false;
+			return result;
+		}
+		catch (const std::exception& e) {
+			result.error = e.what();
+			result.success = false;
+			return result;
+		}
+	}
+};
+
+unzip::unzip() : d_(*new impl()) {}
+unzip::~unzip() {
+	if (d_.fut_.valid())
+		d_.fut_.get();
+
+	delete& d_;
+}
+
+void unzip::start(const std::string& filename,
+	const std::string& directory) {
+	if (unzipping()) {
+		// allow only one instance
+		return;
+	}
+
+	d_.filename_ = filename;
+	d_.directory_ = directory;
+
+	// run task asynchronously
+	d_.fut_ = std::async(std::launch::async, d_.unzip_func, &d_);
+	return;
+}
+
+bool unzip::unzipping() {
+	if (d_.fut_.valid())
+		return d_.fut_.wait_for(std::chrono::seconds{ 0 }) != std::future_status::ready;
+	else
+		return false;
+}
+
+bool unzip::result(unzip_log& log, std::string& error) {
+	error.clear();
+	log = {};
+
+	if (unzipping()) {
+		error = "Task not yet complete";
+		return false;
+	}
+
+	if (d_.fut_.valid()) {
+		auto result = d_.fut_.get();
+		error = result.error;
+		log = d_.log_;
+		return result.success;
+	}
+
+	error = "unexpected error";
+	log = d_.log_;
+	return false;
+}
