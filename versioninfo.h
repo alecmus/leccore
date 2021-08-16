76d5c805710d63f56f835104fd374d57b609dd64
Alec Musasa <alecmus@live.com>
2021-08-17 00:25:19 +0200

    Update versioninfo.h

--- versioninfo.h
+++ versioninfo.h
@@ -17,9 +17,9 @@
 #define leccorecompany		"Alec Musasa"
 #define leccorefilename		"leccore.dll"
 
-#define leccoreversion		"1.0.0 alpha 4"
+#define leccoreversion		"1.0.0 alpha 5"
 #define leccoremajor		1
 #define leccoreminor		0
 #define leccorerevision		0
 
-#define leccoredate			"16 Aug 2021"
+#define leccoredate			"17 Aug 2021"
