9e3eab9e35ced450b71f31547d66b47b7bcadfce
Alec Musasa <alecmus@live.com>
2022-01-01 18:40:37 +0200

    Interface improvement to the image class.

--- image.h
+++ image.h
@@ -39,6 +39,71 @@ namespace liblec {
 				bmp,
 			};
 
+			/// <summary>Image options.</summary>
+			class leccore_api image_options {
+				image::format _format = image::format::jpg;
+				image_quality _quality = image_quality::medium;
+				leccore::size _size;
+				bool _enlarge_if_smaller = false;
+				bool _keep_aspect_ratio = true;
+				bool _crop = false;
+
+			public:
+				/// <summary>Get or set the image format.</summary>
+				/// <returns>A reference to the property.</returns>
+				image::format& format();
+
+				/// <summary>Set the image format.</summary>
+				/// <param name="format">The image format, as defined in <see cref='image::format'></see>.</param>
+				/// <returns>A reference to the modified object.</returns>
+				image_options& format(const image::format& format);
+
+				/// <summary>Get or set the image resampling quality.</summary>
+				/// <returns>A reference to the property.</returns>
+				image_quality& quality();
+
+				/// <summary>Set the image resampling quality.</summary>
+				/// <param name="quality">The quality to use when resampling, as defined in <see cref='image_quality'></see>.</param>
+				/// <returns>A reference to the modified object.</returns>
+				image_options& quality(const image_quality& quality);
+
+				/// <summary>Get or set the image target size.</summary>
+				/// <returns>A reference to the property.</returns>
+				leccore::size& size();
+
+				/// <summary>Set the image target size.</summary>
+				/// <param name="size">The target size of the image, as defined in <see cref='leccore::size'></see>.</param>
+				/// <returns>A reference to the modified object.</returns>
+				image_options& size(const leccore::size& size);
+
+				/// <summary>Get or set whether to enlarge the image if it is smaller than the target size.</summary>
+				/// <returns>A reference to the property.</returns>
+				bool& enlarge_if_smaller();
+
+				/// <summary>Set whether to enlarge the image if it is smaller than the target size.</summary>
+				/// <param name="enlarge_if_smaller">Whether to enlarge the image if it is smaller than the target size.</param>
+				/// <returns>A reference to the modified object.</returns>
+				image_options& enlarge_if_smaller(const bool& enlarge_if_smaller);
+
+				/// <summary>Get or set whether to keep the image aspect ratio.</summary>
+				/// <returns>A reference to the property.</returns>
+				bool& keep_aspect_ratio();
+
+				/// <summary>Set whether to keep the image aspect ratio.</summary>
+				/// <param name="keep_aspect_ratio">Whether to keep the image aspect ratio.</param>
+				/// <returns>A reference to the modified object.</returns>
+				image_options& keep_aspect_ratio(const bool& keep_aspect_ratio);
+
+				/// <summary>Get or set whether to crop the image.</summary>
+				/// <returns>A reference to the property.</returns>
+				bool& crop();
+
+				/// <summary>Set whether to crop the image.</summary>
+				/// <param name="crop">Whether to crop the image.</param>
+				/// <returns>A reference to the modified object.</returns>
+				image_options& crop(const bool& crop);
+			};
+
 			/// <summary>Save the image to file as-is.</summary>
 			/// <param name="full_path">The full path to the destination file. On a successful save the
 			/// 'actual' path is written back to this parameter.</param>
@@ -52,22 +117,11 @@ namespace liblec {
 			/// <summary>Save the image to file, resampling it to a defined size.</summary>
 			/// <param name="full_path">The full path to the destination file. On a successful save the
 			/// 'actual' path is written back to this parameter.</param>
-			/// <param name="format">The image format, as defined in <see cref='image::format'></see>.</param>
-			/// <param name="quality">The quality to use when resampling, as defined in <see cref='image_quality'></see>.</param>
-			/// <param name="size">The size of the image, as defined in <see cref='leccore::size'></see>.</param>
-			/// <param name="crop">Whether to crop the image.</param>
-			/// <param name="keep_aspect_ratio">Whether to keep the aspect ratio of the image.</param>
-			/// <param name="enlarge_if_smaller">Whether to enlarge the image if the target size is smaller
-			/// than the current size of the image.</param>
+			/// <param name="options">The image options, as defined in <see cref='image_options'></see>.</param>
 			/// <param name="error">Error information.</param>
 			/// <returns>Returns true if successful, else false.</returns>
 			bool save(std::string& full_path,
-				image::format format,
-				image_quality quality,
-				leccore::size size,
-				bool crop,
-				bool keep_aspect_ratio,
-				bool enlarge_if_smaller,
+				image_options& options,
 				std::string& error);
 
 		private:
--- image/image.cpp
+++ image/image.cpp
@@ -55,6 +55,43 @@ public:
 	}
 };
 
+image::format& image::image_options::format() { return _format; }
+image::image_options& image::image_options::format(const image::format& format) {
+	_format = format;
+	return *this;
+}
+
+image_quality& image::image_options::quality() { return _quality; }
+image::image_options& image::image_options::quality(const image_quality& quality) {
+	_quality = quality;
+	return *this;
+}
+
+liblec::leccore::size& image::image_options::size() { return _size; }
+image::image_options& image::image_options::size(const leccore::size& size) {
+	_size = size;
+	return *this;
+}
+
+bool& image::image_options::enlarge_if_smaller() { return _enlarge_if_smaller; }
+image::image_options& image::image_options::enlarge_if_smaller(const bool& enlarge_if_smaller) {
+	_enlarge_if_smaller = enlarge_if_smaller;
+	return *this;
+}
+
+bool& image::image_options::keep_aspect_ratio() { return _keep_aspect_ratio; }
+image::image_options& image::image_options::keep_aspect_ratio(const bool& keep_aspect_ratio) {
+	_keep_aspect_ratio = keep_aspect_ratio;
+	return *this;
+}
+
+bool& image::image_options::crop() { return _crop; }
+
+image::image_options& liblec::leccore::image::image_options::crop(const bool& crop) {
+	_crop = crop;
+	return *this;
+}
+
 image::image(int png_resource) : _d(*new impl(png_resource, "")) {}
 image::image(const std::string& file) : _d(*new impl(0, file)) {}
 image::~image() { delete& _d; }
@@ -63,13 +100,10 @@ bool image::save(std::string& full_path, image::format format, std::string& erro
 	return gdiplus_bitmap_to_file(_d.get(), full_path, format, error);
 }
 
-bool image::save(std::string& full_path, image::format format, image_quality quality,
-	leccore::size size, bool crop, bool keep_aspect_ratio, bool enlarge_if_smaller, std::string& error) {
+bool image::save(std::string& full_path, image_options& options, std::string& error) {
 	leccore::size final_size;
-	auto resized_image = resize_gdiplus_bitmap(_d.get(), size, keep_aspect_ratio, quality, enlarge_if_smaller, crop, final_size);
-	bool success = gdiplus_bitmap_to_file(resized_image, full_path, format, error);
-	if (success)
-		delete resized_image;
-	
+	auto resized_image = resize_gdiplus_bitmap(_d.get(), options.size(), options.keep_aspect_ratio(), options.quality(), options.enlarge_if_smaller(), options.crop(), final_size);
+	bool success = gdiplus_bitmap_to_file(resized_image, full_path, options.format(), error);
+	delete resized_image;
 	return success;
 }
