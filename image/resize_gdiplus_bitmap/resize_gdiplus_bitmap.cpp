20346f4c47f9582e0ff16dfca1fa6b8cb303552d
Alec Musasa <alecmus@live.com>
2022-01-02 16:04:54 +0200

    Improvement to gdiplus_bitmap_to_file.

--- image/resize_gdiplus_bitmap/resize_gdiplus_bitmap.cpp
+++ image/resize_gdiplus_bitmap/resize_gdiplus_bitmap.cpp
@@ -205,7 +205,6 @@ bool gdiplus_bitmap_to_file(Gdiplus::Bitmap* bitmap,
 	}
 
 	if (bitmap) {
-		// save resized bitmap to file
 		CLSID enc_id;
 		std::wstring mimetype;
 
@@ -228,32 +227,20 @@ bool gdiplus_bitmap_to_file(Gdiplus::Bitmap* bitmap,
 		}
 
 		if (get_encoder_clsid(mimetype, enc_id) > -1) {
-			switch (format) {
-			case liblec::leccore::image::format::png: {
-				// save resized bitmap to file
-				Gdiplus::Status status = bitmap->Save(liblec::leccore::convert_string(full_path).c_str(), &enc_id);
+			// check if there is an alpha channel
+			const bool source_has_alpha_channel = bitmap->GetPixelFormat() & PixelFormatAlpha;
+			const bool destination_has_alpha_channel = format == liblec::leccore::image::format::png;
 
-				if (status != Gdiplus::Status::Ok) {
-					error.assign(liblec::leccore::get_gdiplus_status_info(&status));
-					success = false;
-				}
-				else
-					success = true;
-			}
-			break;
-
-			case liblec::leccore::image::format::bmp:
-			case liblec::leccore::image::format::jpg:
-			default: {
+			if (source_has_alpha_channel && !destination_has_alpha_channel) {
 				// clone the bitmap
 				Gdiplus::Bitmap* bmp_out = bitmap->Clone(0, 0, bitmap->GetWidth(), bitmap->GetHeight(), bitmap->GetPixelFormat());
 				Gdiplus::Graphics graphics(bmp_out);
 
-				// clear background in case source is in PNG and has an alpha channel
+				// modify clone by clearing the background
 				graphics.Clear(Gdiplus::Color::White);
 				graphics.DrawImage(bitmap, 0, 0);
 
-				// save resized bitmap to file
+				// save the modified clone to file
 				Gdiplus::Status status = bmp_out->Save(liblec::leccore::convert_string(full_path).c_str(), &enc_id);
 
 				if (status != Gdiplus::Status::Ok) {
@@ -263,9 +250,19 @@ bool gdiplus_bitmap_to_file(Gdiplus::Bitmap* bitmap,
 				else
 					success = true;
 
+				// cleanup the clone
 				delete bmp_out;
 			}
-			break;
+			else {
+				// save bitmap to file
+				Gdiplus::Status status = bitmap->Save(liblec::leccore::convert_string(full_path).c_str(), &enc_id);
+
+				if (status != Gdiplus::Status::Ok) {
+					error.assign(liblec::leccore::get_gdiplus_status_info(&status));
+					success = false;
+				}
+				else
+					success = true;
 			}
 		}
 	}
